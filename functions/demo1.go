c19a8ded76a222e6503c5ae9b9dadd4da5ee2dcd
kubra-bulut <kubrabuulut@gmail.com>
2024-07-31 22:00:36 +0300

    functions and maps added

---
+++ functions/demo1.go
@@ -0,0 +1,6 @@
+package functions
+
+func Topla(sayi1 int, sayi2 int) int {
+	var toplam = sayi1 + sayi2
+	return toplam
+}
--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "golesson/slices"
+import (
+	"fmt"
+	"golesson/functions"
+)
 
 func main() {
 	//variables.Demo1()
 	//conditionals.Demo3()
 	//loops.Demo6()
-	slices.Demo2()
+	//slices.Demo2()
+
+	//var sonuc = functions.Topla(5, 6)
+	//fmt.Println(sonuc)
+
+	//sonuc1, sonuc2, sonuc3, sonuc4 := functions.DortIslem(10, 6)
+	//fmt.Println("toplam: ", sonuc1)
+	//fmt.Println("Çıkarım: ", sonuc2)
+	//fmt.Println("Çarpım: ", sonuc3)
+	//fmt.Println("Bölüm: ", sonuc4)
+
+	fmt.Println(functions.ToplaVariadic(5, 3, 2, 1))
 }
